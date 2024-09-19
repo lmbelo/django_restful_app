a45e80926717be3d23b099dc387a8b23ed235929
Lucas Belo <lmbelo@Lucass-MacBook-Pro.local>
2024-09-19 20:55:17 -0300

    delete_purchased and mark_bulk_purchased custom actions added to ShoppingItemViewSet

--- shopping_list/api/serializers.py
+++ shopping_list/api/serializers.py
@@ -6,4 +6,5 @@ class ShoppingItemSerializer(serializers.ModelSerializer):
     class Meta:
 
         model = ShoppingItem
-        fields = ["id", "name", "purchased"] 
+        fields = ["id", "name", "purchased"]
+        read_only_fields = ("id",)
