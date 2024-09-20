2c09549ccd5d6011704228749c3e28d71b30189a
Lucas Belo <lmbelo@Lucass-MacBook-Pro.local>
2024-09-20 09:28:10 -0300

    Multiple shopping lists

--- shopping_list/api/serializers.py
+++ shopping_list/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from shopping_list.models import ShoppingItem
+from shopping_list.models import ShoppingItem, ShoppingList
 
 class ShoppingItemSerializer(serializers.ModelSerializer):
 
@@ -8,3 +8,20 @@ class ShoppingItemSerializer(serializers.ModelSerializer):
         model = ShoppingItem
         fields = ["id", "name", "purchased"]
         read_only_fields = ("id",)
+
+
+    def create(self, validated_data, **kwargs):
+
+        validated_data['shopping_list_id'] = self.context['request'].parse_context['kwargs']['pk']
+        return super(ShoppingItemSerializer, self).create(validated_data)
+
+
+class ShoppingListSerializer(serializers.ModelSerializer):
+
+    shopping_items = ShoppingItemSerializer(many=True, read_only=True)
+
+
+    class Meta:
+
+        model = ShoppingList
+        fields = ['id', 'name', 'shopping_items']
