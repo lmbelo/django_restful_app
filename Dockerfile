048fda039bf48c08287d1ec6fcd0bbef46bd1305
Lucas Belo <lmbelo@Lucass-MacBook-Pro.local>
2024-09-21 13:38:34 -0300

    Render deployment

--- Dockerfile
+++ Dockerfile
@@ -73,4 +73,4 @@ RUN chown -R app:app $HOME
 USER app
 
 # serve the application
-CMD gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
+CMD ./start_app.sh
--- core/settings.py
+++ core/settings.py
@@ -35,9 +35,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", default=get_random_secret_key())
 DEBUG = int(os.environ.get("DEBUG", default=0))
-ALLOWED_HOSTS = os.environ.get(
-    "DJANGO_ALLOWED_HOSTS", default="127.0.0.1 localhost [::1]"
-).split(" ")
+ALLOWED_HOSTS = os.environ.get("RENDER_EXTERNAL_HOSTNAME", default="127.0.0.1 [::1]").split()
+CSRF_TRUSTED_ORIGINS = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
