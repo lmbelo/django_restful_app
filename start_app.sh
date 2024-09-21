db52299b4c83ddd58d4600d9b30d8ea3519bbcbb
Lucas Belo <lmbelo@Lucass-MacBook-Pro.local>
2024-09-21 15:09:18 -0300

    Checking env variable

--- start_app.sh
+++ start_app.sh
@@ -2,6 +2,7 @@
 # exit on error
 set -o errexit
 echo "Running migrations, creating super user, and collecting static"
+echo "RENDER_EXTERNAL_HOSTNAME is set to $RENDER_EXTERNAL_HOSTNAME";
 python manage.py migrate
 python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL || echo "Superuser already exists"
 echo "Ready for app server"
